1644fe66042303cd54991a85160110021535b237
Priya Singh <priyasingh030917@gmail.com>
2026-08-24 21:14:37 +0530

    Time: 143 ms (49.87%), Space: 20 MB (16.99%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
@@ -3,10 +3,12 @@ class Solution:
         st = 0
         m = 0
         d = {}
+
         for i, v in enumerate(s):
             if v in d and st <= d[v]:
                 st = d[v] + 1
             else:
                 m = max(m, i - st + 1)
             d[v] = i
+            
         return m
