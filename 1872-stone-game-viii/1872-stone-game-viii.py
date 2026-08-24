d25c307fc8184e305ce141eb2da43a208eb1bbe5
Priya Singh <priyasingh030917@gmail.com>
2026-08-24 20:37:02 +0530

    Time: 663 ms (75.15%), Space: 32.3 MB (95.27%) - LeetHub

--- 1872-stone-game-viii/1872-stone-game-viii.py
+++ 1872-stone-game-viii/1872-stone-game-viii.py
@@ -8,6 +8,7 @@ class Solution:
         ans=sum_stones[-1] #Start ans with the total sum of all stones
 
         for i in range(len(stones)-2,0,-1):  #We are checking prefix positions:prefix[3] prefix[2] prefix[1] #We don't use prefix[0] because the game requires taking at least 2 stones.
+        
             ans=max(ans,sum_stones[i]-ans) #For each prefix, calculate the score assuming the opponent plays optimally, and keep the best score for Alice
 
         return ans
@@ -15,20 +16,3 @@ class Solution:
 
 
         
-        # #Wrong code
-        # alice_score=0
-        # bob_score=0
-        # ans=[]
-        # for i in stones:
-        #     bob_score+=i
-        # for i in stones:
-        #     alice_score+=i
-        #     #bob_score-=i
-        #     ans.append(alice_score-bob_score)
-        # maximum=ans[0]
-        # for i in ans:
-        #     if abs(maximum)<abs(i):
-        #         maximum=i
-        # return maximum
-
-
