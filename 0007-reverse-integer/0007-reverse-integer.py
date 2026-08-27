b433fbbae9487dbcf306af6c4bfbdf54d05f12cb
Priya Singh <priyasingh030917@gmail.com>
2026-08-27 11:03:59 +0530

    Time: 42 ms (79.52%), Space: 19.2 MB (31.77%) - LeetHub

--- 0007-reverse-integer/0007-reverse-integer.py
+++ 0007-reverse-integer/0007-reverse-integer.py
@@ -8,10 +8,9 @@ class Solution:
             digit=x%10
             rev=rev*10+digit
             x=x//10
-
         rev*=sign
 
         if rev<-2**31 or rev>2**31-1:
             return 0
-            
+
         return rev
