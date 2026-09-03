16ad89d69c9bff1d3162db3e2600ad4bffb2d8ad
Priya Singh <priyasingh030917@gmail.com>
2026-09-03 10:10:10 +0530

    Time: 7 ms (5.35%), Space: 20 MB (10.06%) - LeetHub

--- 0905-sort-array-by-parity/0905-sort-array-by-parity.py
+++ 0905-sort-array-by-parity/0905-sort-array-by-parity.py
@@ -1,7 +1,6 @@
 class Solution:
     def sortArrayByParity(self, nums: List[int]) -> List[int]:
         ans=[]
-        
         for i in nums:
             if i%2!=0:
                 ans.append(i)
