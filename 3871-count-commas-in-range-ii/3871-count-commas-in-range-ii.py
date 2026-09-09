01b8a8a9f3a7c03f5bca93836a8f92f7993be270
Priya Singh <priyasingh030917@gmail.com>
2026-09-09 12:03:06 +0530

    Time: 0 ms (100%), Space: 19.3 MB (51.7%) - LeetHub

--- 3871-count-commas-in-range-ii/3871-count-commas-in-range-ii.py
+++ 3871-count-commas-in-range-ii/3871-count-commas-in-range-ii.py
@@ -2,26 +2,15 @@ class Solution:
     def countCommas(self, n: int) -> int:
         if n<10**3:
             return 0
-        count=0
-        num=1000
-        while num<=n:
-            count+=n-num+1
-            num*=1000
-        return count
-
-# class Solution:
-#     def countCommas(self, n: int) -> int:
-#         if n<10**3:
-#             return 0
-#         elif n<10**6:
-#             return n-999
-#         elif n<10**9:
-#             return 2*(n-10**6+1)+10**6 - 1000
-#         elif n<10**12:
-#             return 3*(n-10**9+1)+2*(10**9-10**6)+10**6 - 1000
-#         elif n < 10**15:
-#             return 4*(n-10**12+1)+3*(10**12-10**9)+2*(10**9-10**6)+10**6 - 1000
-#         return 5*(n - 10**15 + 1) + 4*(10**15 - 10**12) + 3*(10**12 - 10**9) + 2*(10**9 - 10**6) + 10**6 - 1000
+        elif n<10**6:
+            return n-999
+        elif n<10**9:
+            return 2*(n-10**6+1)+10**6 - 1000
+        elif n<10**12:
+            return 3*(n-10**9+1)+2*(10**9-10**6)+10**6 - 1000
+        elif n < 10**15:
+            return 4*(n-10**12+1)+3*(10**12-10**9)+2*(10**9-10**6)+10**6 - 1000
+        return 5*(n - 10**15 + 1) + 4*(10**15 - 10**12) + 3*(10**12 - 10**9) + 2*(10**9 - 10**6) + 10**6 - 1000
             
             
         
