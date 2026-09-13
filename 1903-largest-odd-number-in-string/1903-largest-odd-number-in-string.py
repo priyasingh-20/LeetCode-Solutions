aa748ac922905012fbb61479a48c74bb05549b60
Priya Singh <priyasingh030917@gmail.com>
2026-09-13 20:27:04 +0530

    Time: 5 ms (70.7%), Space: 20.3 MB (30.68%) - LeetHub

--- 1903-largest-odd-number-in-string/1903-largest-odd-number-in-string.py
+++ 1903-largest-odd-number-in-string/1903-largest-odd-number-in-string.py
@@ -3,7 +3,8 @@ class Solution:
 
         for i in range(len(num)-1,-1,-1):
             if int(num[i])%2!=0:
-                return num[0:i+1]    
+                return num[0:i+1] 
+                   
         return ""
 
 
