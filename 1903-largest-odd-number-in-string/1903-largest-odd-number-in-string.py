5bf5323f0e1ebec7773ccc0aa1149175fae568c9
Priya Singh <priyasingh030917@gmail.com>
2026-09-13 20:25:24 +0530

    Time: 7 ms (59.8%), Space: 20.1 MB (64.29%) - LeetHub

--- 1903-largest-odd-number-in-string/1903-largest-odd-number-in-string.py
+++ 1903-largest-odd-number-in-string/1903-largest-odd-number-in-string.py
@@ -3,19 +3,11 @@ class Solution:
         for i in range(len(num)-1,-1,-1):
             if int(num[i])%2!=0:
                 return num[0:i+1]
+                
         return ""
 
 
-        # n=len(num)
-        # while n:
-        #     if int(num[-1])%2!=0:
-        #         return num
-        #         break
-        #     else:
-        #         num=num[0:n-1]
-        #         n-=1
-        # return ""
-
+       
         
         
         
