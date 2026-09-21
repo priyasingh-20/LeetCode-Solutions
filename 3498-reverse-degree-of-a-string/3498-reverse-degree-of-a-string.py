5db9f655567be80939c208aac4215852029aa3d4
Priya Singh <priyasingh030917@gmail.com>
2026-09-22 00:52:53 +0530

    Time: 7 ms (69.58%), Space: 19.1 MB (87.18%) - LeetHub

---
+++ 3498-reverse-degree-of-a-string/3498-reverse-degree-of-a-string.py
@@ -0,0 +1,8 @@
+class Solution:
+    def reverseDegree(self, s: str) -> int:
+        st="abcdefghijklmnopqrstuvwxyz"
+        ts=st[::-1]
+        total=0
+        for i in range(len(s)):
+            total+=(ts.index(s[i])+1)*(i+1)
+        return total
