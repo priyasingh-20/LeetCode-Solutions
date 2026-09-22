200234b7bbe7313221736c235bba90ffbd71b0cc
Priya Singh <priyasingh030917@gmail.com>
2026-09-23 00:09:05 +0530

    Time: 39 ms (91.22%), Space: 23.6 MB (20.86%) - LeetHub

---
+++ 0214-shortest-palindrome/0214-shortest-palindrome.py
@@ -0,0 +1,18 @@
+class Solution:
+    def shortestPalindrome(self, s: str) -> str:
+        rev=s[::-1]
+        temp=s+"#"+rev
+
+        lps=[0]*len(temp)
+        
+        j=0
+        for i in range(1,len(temp)):
+            while j>0 and temp[i]!=temp[j]:
+                j=lps[j-1]
+
+            if temp[i]==temp[j]:
+                j+=1
+                lps[i]=j
+
+        longest=lps[-1]
+        return rev[:len(s)-longest]+s
